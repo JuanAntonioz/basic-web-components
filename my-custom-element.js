9c83c1b373e7a3f8951ce48e01deb461e4a29f29
Antonioz <1317580+Astronatus@users.noreply.github.com>
2026-01-13 14:16:25 -0500

    feat: add custom template

--- my-custom-element.js
+++ my-custom-element.js
@@ -1,10 +1,19 @@
-
+const template = document.createElement("div");
+template.innerHTML = `
+  <style>
+      h1 {
+        color: skyblue;
+      }
+  </style>
+  <h1>title</h1>
+  <p>text message</p>
+`
 class MyCustomElement extends HTMLElement {
     constructor() {
         /**
          * Provide access to the methods and properties of the HTMLElement class
          */
-        super();       
+        super();
 
         this.p = document.createElement('p');
     }
@@ -12,6 +21,7 @@ class MyCustomElement extends HTMLElement {
     connectedCallback() {
         this.p.textContent = "Message";
         this.appendChild(this.p);
+        this.appendChild(template)
     }
 }
 
