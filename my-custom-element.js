da0cabbfd72d005f36812a708351a3fce9527b79
Antonioz <1317580+Astronatus@users.noreply.github.com>
2026-01-13 14:41:41 -0500

    chore: add biome support

--- my-custom-element.js
+++ my-custom-element.js
@@ -7,22 +7,22 @@ template.innerHTML = `
   </style>
   <h1>title</h1>
   <p>text message</p>
-`
+`;
 class MyCustomElement extends HTMLElement {
-    constructor() {
-        /**
-         * Provide access to the methods and properties of the HTMLElement class
-         */
-        super();
+	constructor() {
+		/**
+		 * Provide access to the methods and properties of the HTMLElement class
+		 */
+		super();
 
-        this.p = document.createElement('p');
-    }
+		this.p = document.createElement("p");
+	}
 
-    connectedCallback() {
-        this.p.textContent = "Message";
-        this.appendChild(this.p);
-        this.appendChild(template)
-    }
+	connectedCallback() {
+		this.p.textContent = "Message";
+		this.appendChild(this.p);
+		this.appendChild(template);
+	}
 }
 
-customElements.define('my-custom-element', MyCustomElement)
+customElements.define("my-custom-element", MyCustomElement);
