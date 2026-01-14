7a6c8b75c8738f28e8d9a7bd0597bd31e3d6fedd
Antonioz <1317580+Astronatus@users.noreply.github.com>
2026-01-14 00:03:41 -0500

    feat: add attributes and custom styles

--- my-custom-element.js
+++ my-custom-element.js
@@ -8,17 +8,29 @@ class MyCustomElement extends HTMLElement {
 		this.attachShadow({
 			mode: "open",
 		});
+
+    this.img = this.getAttribute("img")
 	}
 
 	getTemplate() {
 		const template = document.createElement("template");
 		template.innerHTML = `
-      <section>
-        <h2>
-          <slot name="title"></slot>
-        </h2>
+      <section class="product-card-container">
         <div>
-          <slot name="body"></slot>
+          <img src="${this.img}" alt="image" class="product-card-image"/>
+        </div>
+        <header class="product-card-header">
+          <h2 class="product-card-title">
+            <slot name="product-name"></slot>
+          </h2>
+        <header>
+        <div class="product-card-body">
+          <span class="product-card-price product-card-price__list-price">
+            $ <slot name="product-list-price"></slot>
+          </span>
+          <span class="product-card-price  product-card-price__selling-price">
+            $ <slot name="product-selling-price"></slot>
+          </span>
         </div>
       </section>
       ${this.getStyles()}
@@ -29,8 +41,45 @@ class MyCustomElement extends HTMLElement {
 	getStyles() {
 		return `
       <style>
-        h2 {
-          color: brown;
+        body, html {
+          font-family: sans-serif, tahoma, arial;
+          font-size: 16px;
+        }
+        * {
+          box-sizing: border-box;
+        }
+        .product-card-container * {
+          font-family: sans-serif, tahoma, arial;
+          color: #363842;
+        }
+        .product-card-container {
+          max-width: 20rem;
+          background-color: #FFFFFF;
+          border-radius: 10px;
+          border: 1px solid #D8D9E0;
+          padding: 2.5rem;
+        }
+        .product-card-image {
+          border-radius: 10px;
+          width: 100%;
+        }
+        .product-card-title {
+          font-size: 1.6rem;
+          font-weight: normal;
+        }
+        .product-card-body {
+          display: flex;
+          flex-direction: column;
+          gap: 0.5rem;
+        }
+        .product-card-price {
+        }
+        .product-card-price__list-price {
+          font-size: 1.6rem;
+          text-decoration: line-through;
+        }
+        .product-card-price__selling-price{
+          font-size: 2.9rem;
         }
       </style>
     `;
