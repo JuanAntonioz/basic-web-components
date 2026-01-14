c7deea00c86296182a9ce424286c3747892c1fbb
Antonioz <1317580+Astronatus@users.noreply.github.com>
2026-01-14 12:15:41 -0500

    feat: add slotted example

---
+++ examples/slotted/app.js
@@ -0,0 +1,53 @@
+class MyComponent extends HTMLElement {
+  constructor () {
+    super();
+    this.attachShadow({
+      mode: "open"
+    })
+  }
+
+  getStyles() {
+    console.log("styles")
+    return `
+      <style>
+        h1 {
+          color: brown;
+          font-size: 16px;
+        }
+        p {
+          color: #5d5c5c;
+        }
+        ::slotted(span) {
+          font-size: 40px;
+        }
+        ::slotted(.text) {
+          font-size: 28px;
+        }
+      </style>
+    `
+  }
+
+  getTemplate() {
+    console.log("template")
+    const template = document.createElement("template")
+    template.innerHTML = `
+      <div>
+        <h1><slot name="title"></slot></h1>
+        <p><slot name="body"></slot></p>
+      </div>
+      ${this.getStyles()}
+    `
+    return template;
+  }
+
+  render() {
+    this.shadowRoot.appendChild(this.getTemplate().content.cloneNode(true))
+  }
+
+  connectedCallback(){
+    this.render();
+  }
+
+}
+
+customElements.define("my-component", MyComponent)
